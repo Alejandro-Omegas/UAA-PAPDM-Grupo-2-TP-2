76352417b556c691c6b32da091d12624437b67ce
Alvaro Torales <alvaro_torales@hotmail.com>
2024-12-16 16:22:54 -0300

    Actualizar Aboutscreen y Theme.kt

--- app/src/main/java/com/alejandroarriola/uaa_papdm_grupo_2_tp_2/ui/theme/Theme.kt
+++ app/src/main/java/com/alejandroarriola/uaa_papdm_grupo_2_tp_2/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.alejandroarriola.uaa_papdm_grupo_2_tp_2.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,13 +7,9 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
-import androidx.compose.material3.Typography
 
 // Colores del tema
 private val DarkColorScheme = darkColorScheme(
@@ -29,31 +24,9 @@ private val LightColorScheme = lightColorScheme(
     tertiary = Pink40
 )
 
-val Typography = Typography(
-    // Define tu propia tipografía aquí
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontSize = 16.sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontSize = 14.sp
-    ),
-    bodySmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontSize = 12.sp
-    ),
-    headlineMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontSize = 20.sp,
-        fontWeight = androidx.compose.ui.text.font.FontWeight.Bold
-    ),
-)
-
 @Composable
 fun UAAPAPDMGrupo2TP2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -62,14 +35,14 @@ fun UAAPAPDMGrupo2TP2Theme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = Typography, // Usa Typography de MaterialTheme
         content = content
     )
 }
+
