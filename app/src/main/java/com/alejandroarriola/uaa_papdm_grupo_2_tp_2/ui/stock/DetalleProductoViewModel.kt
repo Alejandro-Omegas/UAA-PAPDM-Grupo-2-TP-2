5ca9189b12c8247dd84b2cff0070e17775277721
daixmnz <66316995+daixmnz@users.noreply.github.com>
2024-12-16 22:12:30 -0300

    Update DetalleProductoViewModel.kt

--- app/src/main/java/com/alejandroarriola/uaa_papdm_grupo_2_tp_2/ui/stock/DetalleProductoViewModel.kt
+++ app/src/main/java/com/alejandroarriola/uaa_papdm_grupo_2_tp_2/ui/stock/DetalleProductoViewModel.kt
@@ -2,7 +2,14 @@ package com.alejandroarriola.uaa_papdm_grupo_2_tp_2.ui.stock
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.alejandroarriola.uaa_papdm_grupo_2_tp_2.data.StockRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 data class DetalleProductoUiState(
     val agotado: Boolean = true,
@@ -13,15 +20,45 @@ class DetalleProductoViewModel(
     savedStateHandle: SavedStateHandle,
     private val stockRepository: StockRepository
 ) : ViewModel() {
-    /*TODO:
-    private val productoId
+    private val productoId: Int = checkNotNull(savedStateHandle[DetalleProductoDestino.idProductoArg]) //explicitamante declarar como Int o el IDE lo identifica como "Any" type
+    val uiState: StateFlow<DetalleProductoUiState> =
+        stockRepository.obtenerStockPorId(productoId)
+            .filterNotNull()
+            .map {
+                DetalleProductoUiState(agotado = it.cantidad <= 0, productoDetalles = it.toProductoDetalles())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetalleProductoUiState()
+            )
 
-    val uiState
+    //funciones
+    suspend fun eliminarProducto() {
+        stockRepository.eliminarStock(uiState.value.productoDetalles.toProducto())
+    }
 
-    suspend fun eliminarProducto()
+    fun reducirUno() {
+        viewModelScope.launch {
+            val productoActual = uiState.value.productoDetalles.toProducto()
 
-    fun aumentarUno()
+            if(productoActual.cantidad > 0) {
+                stockRepository.actualizarStock(productoActual.copy(cantidad = productoActual.cantidad - 1))
+            }
+        }
+    }
 
-    fun reducirUno()
-     */
+    fun aumentarUno() {
+        viewModelScope.launch {
+            val productoActual = uiState.value.productoDetalles.toProducto()
+
+            if(productoActual.cantidad < 2147483647) { //max Int value
+                stockRepository.actualizarStock(productoActual.copy(cantidad = productoActual.cantidad + 1))
+            }
+        }
+    }
+
+    //singletons
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
